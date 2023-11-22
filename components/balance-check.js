5043c5be1327e4dd781618d7f024fce8e3f76f20
bitmover-studio <bitmover@protonmail.ch>
2023-11-22 01:40:30 -0300

    notifcation when balance changes

--- components/balance-check.js
+++ components/balance-check.js
@@ -11,18 +11,17 @@ textArea.addEventListener("keyup", function () {
 });
 let addressesList = textArea.value.split("\n");
 
-
 // Get Bitcoin Price
 async function getBtcPrice() {
   try {
     try {
-      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,brl');
+      const response = await fetch('https://bitcoindata.science/api/priceusd.json');
       const data = await response.json();
-      let btcPrice = data.bitcoin.usd;
+      let btcPrice = data.price;
       return btcPrice
     }
     catch (error) {
-      console.warn('Coingecko API failed - ' + error)
+      console.warn('bitcoindata API failed - ' + error)
       const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice/BRL.json");
       const data = await response.json();
       let btcPrice = data.bpi.USD.rate_float;
@@ -38,12 +37,12 @@ async function getBtcPrice() {
 let balance = [];
 const outputArea = document.getElementById("balances")
 
-async function getAddressBalance(address, price) {
+async function getAddressBalance(address, price, balancearray) {
   try {
     try {
       const response = await fetch('https://mempool.space/api/address/' + address);
       const data = await response.json();
-      balance.push({
+      balancearray.push({
         address: address,
         balance: (data.chain_stats.funded_txo_sum - data.chain_stats.spent_txo_sum) / 100000000,
         value: (data.chain_stats.funded_txo_sum - data.chain_stats.spent_txo_sum) / 100000000 * price,
@@ -55,7 +54,7 @@ async function getAddressBalance(address, price) {
       console.warn('Mempool API failed - ' + error)
       const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/' + address + '/balance');
       const data = await response.json();
-      balance.push({
+      balancearray.push({
         address: address,
         balance: data.balance / 100000000,
         value: data.balance / 100000000 * price,
@@ -79,11 +78,12 @@ function delay(ms) {
 // Declare an async function that waits for a delay and then calls another async function
 async function getBalances(price) {
   balance = [];
+  modalText = {}
   for (const i of addressesList) {
     // Use await with the delay function
     await delay(500);
     // Call the async function
-    await getAddressBalance(i, price);
+    await getAddressBalance(i, price, balance);
   }
   document.getElementById("submit").innerHTML = 'Get Balance!'
   document.getElementById("submit").disabled = false;
@@ -124,5 +124,58 @@ function handleClick() {
   addressesList = [];
   textArea = document.getElementById('BalanceChecker');
   addressesList = textArea.value.split("\n");
+  addressesList = addressesList.filter(n => n);
   getBtcPrice().then(price => getBalances(price))
+}
+
+// Update Address Balance
+let newBalance = [];
+let modalText = {};
+
+const modal = new bootstrap.Modal(document.getElementById("modal"), {});
+
+async function compareBalance(price) {
+  newBalance = [];
+  for (const i of addressesList) {
+    await delay(500);
+    await getAddressBalance(i, price, newBalance)
+  };
+  for (let i in balance) {
+    if (balance[i].balance !== newBalance[i].balance) {
+      modalText = {
+        address: newBalance[i].address,
+        balance: newBalance[i].balance,
+        value: newBalance[i].value,
+        color: "",
+        title: "New confirmed transaction",
+        body: "Transaction confirmed"
+      };
+    }
+    else if (balance[i].unconfirmed !== newBalance[i].unconfirmed) {
+      modalText = {
+        address: newBalance[i].address,
+        balance: newBalance[i].unconfirmed,
+        value: newBalance[i].unconfirmed_value,
+        color: "text-warning-emphasis",
+        title: "Incoming transaction",
+        body: "Transaction not confirmed yet"
+      };
+    }
+  }
+  document.querySelector('.modal-title').innerHTML = modalText.title;
+  document.querySelector('.modal-address').innerHTML = modalText.address;
+  document.querySelector('.modal-balance').innerHTML = modalText.balance;
+  document.querySelector('.modal-value').innerHTML = modalText.value.toLocaleString("en-US", { style: "currency", currency: "USD" });
+  document.getElementById('modal-body-text').className = modalText.color;
+
+  if (Object.keys(modalText).length){
+    modal.show()
+  }
+  return modalText;
+};
+
+if (!Array.isArray(balance) || !balance.length) {
+  var timerCompareBalance = setInterval(() => {
+  getBtcPrice().then(price => compareBalance(price));
+}, 300000)
 }
