58bade0e61be3dbfb8f1972f36656de5e37a6ea6
bitmover-studio <bitmover@protonmail.ch>
2023-12-13 20:18:20 -0300

    overal fixes and layout changes

--- components/strategy.js
+++ components/strategy.js
@@ -1,21 +1,12 @@
 "use strict";
-// timestamp to human readable date
-function timestampToDate(timestamp) {
-    let date = new Date(timestamp)
-    let year = date.getFullYear();
-    let month = String(date.getMonth() + 1).padStart(2, '0');
-    let day = String(date.getDate()).padStart(2, '0');
-    let dateString = year + "-" + month + "-" + day;
-    return dateString
-}
 
 // Calculate SMA
 function calculateSMA(data, period) {
     let sma = [];
-    for(let i = 0; i < data.length; i++) {
-        if(i >= period) {
+    for (let i = 0; i < data.length; i++) {
+        if (i >= period) {
             let sum = 0;
-            for(let j = i; j > i - period; j--) {
+            for (let j = i; j > i - period; j--) {
                 sum += data[j][1];
             }
             sma.push([data[i][0], sum / period]);
@@ -36,12 +27,14 @@ const btcChart = async () => {
         const data = await response.json()
         let prices = data.prices;
         price = prices[prices.length - 1][1];
+        let yesterdayPrice = prices[prices.length - 2][1];
+        let priceVar = ((price - yesterdayPrice) / yesterdayPrice);
         let sma200 = calculateSMA(prices, 1400);
         sma = sma200[sma200.length - 1][1];
 
         // equalize both array lengths
-        sma200 = sma200.slice(-3000, -1);
-        prices = prices.slice(-3000, -1);
+        prices = prices.slice(1400, -1);
+        sma200 = sma200.slice(1400, -1);
 
         // Load data into Chart
         chart.updateSeries([{
@@ -53,29 +46,31 @@ const btcChart = async () => {
             data: sma200,
             type: "line"
         }]);
-        loadData()
+        // Load Fixed Data
+        let priceAboveSma = (((price - sma) / sma));
+        document.getElementById("pricesma").innerText = priceAboveSma.toLocaleString("en-US", { style: "percent", minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        document.getElementById("sma").innerText = sma.toLocaleString("en-US", { style: "currency", currency: "USD" });
+        document.getElementById("BTCPrice").innerText = price.toLocaleString("en-US", { style: "currency", currency: "USD" });
+        document.getElementById("priceVar").innerText = ( ((priceVar >0) ?'+':'') + priceVar.toLocaleString("en-US", { style: "percent", minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+        (priceVar > 0) ? document.getElementById("priceVar").classList.add('text-success-emphasis') : document.getElementById("priceVar").classList.add('text-danger-emphasis');
+
+        loadData(priceAboveSma);
     }
     catch (error) {
         console.warn(error)
     }
 }
 
-function loadData(){
+function loadData(priceAboveSma) {
     // load data into text-areas
-    document.getElementById("sma").value = sma.toFixed(2);
-    document.getElementById("BTCPrice").value = price.toFixed(2);
-
-    let priceAboveSma = (((price - sma) / sma) * 100).toFixed(2)
-    document.getElementById("pricesma").value = priceAboveSma;
-
     let allowedWithdrawal;
     let wrate = document.getElementById("wrate").value;
     let stash = document.getElementById("stash").value;
 
-    (priceAboveSma > 0.25) ? allowedWithdrawal = (wrate/100/12*stash) : allowedWithdrawal = (wrate/100/12*stash)*0.4;
-
+    (priceAboveSma > 0.25) ? allowedWithdrawal = (wrate / 100 / 12 * stash) : allowedWithdrawal = (wrate / 100 / 12 * stash) * 0.4;
+    document.getElementById("wRateRange").innerHTML = wrate;
     document.getElementById("allowed").value = (allowedWithdrawal).toFixed(8);
-    document.getElementById("allowedvalue").value = (allowedWithdrawal*price).toFixed(2);
+    document.getElementById("allowedvalue").value = (allowedWithdrawal * price).toFixed(2);
 }
 
 let options = {
@@ -99,9 +94,13 @@ let options = {
         theme: 'dark',
         shared: true,
         intersect: false,
+        x: {
+            show: true,
+            format: 'yyyy/MM/dd',
+        },
     },
     grid: {
-        borderColor: "#535A6C",
+        borderColor: "#535A6C33",
         xaxis: {
             lines: {
                 show: true
@@ -117,12 +116,6 @@ let options = {
     },
     yaxis: {
         labels: {
-            /**
-            * Allows users to apply a custom formatter function to yaxis labels.
-            *
-            * @param { String } value - The generated value of the y-axis tick
-            * @param { index } index of the tick / currently executing iteration in yaxis labels array
-            */
             formatter: function (val, index) {
                 return val.toLocaleString("en-US", { style: "currency", currency: "USD" });
             }
