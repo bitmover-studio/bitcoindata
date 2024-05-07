cb9527515ecb98497b14876e07e38daf58e9f69d
bitmover-studio <bitmover@protonmail.ch>
2024-05-06 21:06:19 -0300

    added l0tt0 banner back again

--- components/ad.js
+++ components/ad.js
@@ -7,10 +7,10 @@ class Ad extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <div class="container text-center my-3 mb-5" id="Banner">
-        <a href="https://tinyurl.com/btctalksig" title="BC.Game - official Partner of Argentine National Team & CLOUD9" target="_blank" rel="noreferrer">
-          <img class="border-1 border-dark border img-fluid d-none d-md-inline-block" alt='BC.Game' src="https://bitcoindata.science/img/BC_Game_Cowboy.gif " width='728' height='90'/>
-          <img class="border-1 border-dark border img-fluid d-md-none" alt="BC.Game" src="https://bitcoindata.science/img/BC_Game_Cowboy_mobile.gif" width='320' height='50'/>
+      <div class="container text-center my-3" id="Banner">
+        <a href="https://www.l0tt0.com/" title="l0tt0.com - Bitcoin Games Like You've Never Seen!" target="_blank" rel="noreferrer">
+          <img class="border-1 border-dark border img-fluid d-none d-md-inline-block" alt='l0tt0.com' src="https://bitcoindata.science/img/l0tt0_banner.gif" width='728' height='90'/>
+          <img class="border-1 border-dark border img-fluid d-md-none" alt="l0tt0.com" src="https://bitcoindata.science/img/l0tt0_banner.gif" width='320' height='50'/>
         </a>
         <div class="d-flex justify-content-center align-items-center">
           <p class="small mb-0">Sponsored Content</p>
