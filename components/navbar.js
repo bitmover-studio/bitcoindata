7dec7ffb9f0005847b7814cb64e768290645c0ef
bitmover-studio <bitmover@protonmail.ch>
2025-07-11 23:28:31 -0300

    remove google analytics

--- components/navbar.js
+++ components/navbar.js
@@ -141,25 +141,6 @@ class Navbar extends HTMLElement {
 </div>
       `;
 
-    /* Google Tag */
-    // const script = document.createElement("script");
-    // script.setAttribute("src", "components/googletag.js");
-    // document.head.appendChild(script);
-
-    /* Google tag (gtag.js) --> */
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({
-        "gtm.start": new Date().getTime(),
-        event: "gtm.js",
-      });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-MSMJ4VCD");
 
     function toggler() {
       const storedTheme = localStorage.getItem("theme");
